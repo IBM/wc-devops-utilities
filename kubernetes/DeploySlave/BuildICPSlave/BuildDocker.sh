3fb290b6449bac45e12e0335fab3afbeac203bd7
YI KONG <kongyibj@cn.ibm.com>
2018-07-16 16:48:30 +0800

    add scripts to build ICP DeploySlave

---
+++ kubernetes/DeploySlave/BuildICPSlave/BuildDocker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+helmVersion=v2.6.0
+
+mkdir -p ./commerce-devops-utilities
+cp -r ../../../utilities ./commerce-devops-utilities
+
+#wget -O "./helm.tar.gz" https://storage.googleapis.com/kubernetes-helm/helm-${helmVersion}-linux-amd64.tar.gz
+#tar -zxvf helm.tar.gz
+#cp ./linux-amd64/helm .
+
+if [ $# = 1 ];then
+  docker build -t $1 .
+else
+  docker build -t 'deployslave:latest' .
+fi
+
+rm -rf ./commerce-devops-utilities
+#rm -rf ./helm.tar.gz
+#rm -rf ./helm
+#rm -rf ./linux-amd64
